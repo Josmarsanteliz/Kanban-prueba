1f125064b07c089bf55df016ecd36233646df1cd
Josmar <josmarsanteliz@gmail.com>
2026-09-05 19:18:30 -0400

    feat: preparar backend para despliegue en Render

--- server/server.js
+++ server/server.js
@@ -164,6 +164,7 @@ app.delete('/api/notes/:id', (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log('Servidor corriendo en http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
